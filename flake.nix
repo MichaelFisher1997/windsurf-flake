4bb9cab96033c4fe0d14c8826efe569881871089
MichaelFisher1997 <MichaelFisher1997@users.noreply.github.com>
2025-12-18 09:09:24 +0000

    UPDATE: 1.12.47

--- flake.nix
+++ flake.nix
@@ -18,11 +18,11 @@
         packages = {
           windsurf = pkgs.stdenv.mkDerivation rec {
             pname = "windsurf";
-            version = "1.12.44";
+            version = "1.12.47";
 
             src = pkgs.fetchurl {
-              url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/f93b1c92ecdd92da92e9ae934d52d3098776fc81/Windsurf-linux-x64-1.12.44.tar.gz";
-              sha256 = "1kgs7f9l6vm35ib9nwzzmb1alqzy5frlg8a57j56dh5ydf4cbgkx";
+              url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/8951cd3ad688e789573d7f51750d67ae4a0bea7d/Windsurf-linux-x64-1.12.47.tar.gz";
+              sha256 = "1cwwv34xki6hgw69s3ckrmld595fp5i2bkzr25vxaqkhx4x20dv2";
             };
 
             nativeBuildInputs = with pkgs; [
